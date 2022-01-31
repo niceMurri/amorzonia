804628f7b643394cc5d319dc8db4f7cd19187f7c
niceMurri <77419581+niceMurri@users.noreply.github.com>
2022-01-30 22:20:07 -0300

    Versão 1 de uma landing page.
    Praticando com conhecimentos de HTML, CSS E JS

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+class MobileMenu {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+  }
+
+  init() {
+    this.click();
+  }
+
+  click() {
+    this.mobileMenu.addEventListener("click", (e) => {
+      if (this.navList.classList.contains("active")) {
+        return this.navList.classList.remove("active");
+      }
+
+      this.navList.classList.add("active");
+    });
+  }
+
+  animateLinks() {
+    
+  }
+}
+
+const mobileMenu = new MobileMenu(
+  ".mobile-menu",
+  ".navbar-auth",
+  ".navbar-auth a"
+);
+
+mobileMenu.init();
